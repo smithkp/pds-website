087c38bf2e397fa9b42333c99a924e1e22bc70d4
Kyle <kyles6688@gmail.com>
2019-06-02 23:12:20 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+const server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.use(express.static(path.join(__dirname, 'build')));
 
@@ -8,4 +10,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000);
+app.listen(server_port, server_host);
